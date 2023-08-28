a35cc0c9bff8fa1d50b74305d2e211cd90d0cf29
SerhiiOberemchuk <gardenitparts@gmail.com>
2023-08-28 21:13:13 +0200

    1

---
+++ index.js
@@ -0,0 +1,39 @@
+
+// const message = "Java-Sc,ript - це цікаво";
+// console.log(message.split(" "));
+
+
+
+// const redFruits = ["apple", "strawberry", "cherry", "cranberries"];
+// const fruit = "cherry";
+// // Перевіряємо присутність елемента
+// const hasFruit = redFruits.includes(fruit);
+// if (hasFruit === true) {
+//   console.log(`${fruit} is a red fruit!`);
+// }
+// const serhii = {};
+// serhii.name = 'serii';
+// serhii.age = 38;
+
+// const clients = ["Mango", "Ajax", "Poly", "Kiwi"];
+// console.log(clients.includes("Poly")); // true
+// console.log(clients.includes("Monkong"));
+
+// serhii.height = 185;
+// console.log(serhii);
+
+const raisa = 359;
+switch (raisa)
+{
+    case 20:
+        alert('fgge kgjnbc');
+        break;
+    case 35:
+        alert('ooyeeee');
+        break;
+    case 85:
+        alert('you are old');
+        break;
+    default:
+        alert('you are lacky');
+}
