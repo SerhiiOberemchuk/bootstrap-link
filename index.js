e725683f68cb6be5d3f8a006aa807b585b54b539
SerhiiOberemchuk <gardenitparts@gmail.com>
2023-08-29 21:32:07 +0200

    21

--- index.js
+++ index.js
@@ -22,18 +22,81 @@
 // serhii.height = 185;
 // console.log(serhii);
 
-const raisa = 359;
-switch (raisa)
-{
-    case 20:
-        alert('fgge kgjnbc');
-        break;
-    case 35:
-        alert('ooyeeee');
-        break;
-    case 85:
-        alert('you are old');
-        break;
-    default:
-        alert('you are lacky');
+// const raisa = 359;
+// switch (raisa)
+// {
+//     case 20:
+//         alert('fgge kgjnbc');
+//         break;
+//     case 35:
+//         alert('ooyeeee');
+//         break;
+//     case 85:
+//         alert('you are old');
+//         break;
+//     default:
+//         alert('you are lacky');
+// }
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+
+
+// const keys = [];
+// const values = [];
+// // Change code below this line
+// for (const key in apartment) {
+//   keys = key;
+//   values.push(apartment[key]);
+// };
+// console.log(keys);
+// console.log(values);
+
+// const keys = [];
+// const values = [];
+// const advert = {
+//   service: "apt",
+// };
+// const apartment = Object.create(advert);
+// apartment.descr = "Spacious apartment in the city center";
+// apartment.rating = 4;
+// apartment.price = 2153;
+
+// for (const key in apartment) {
+//   // Change code below this line
+//   if (apartment.hasOwnProperty(key)) {
+//     keys.push(key);
+    
+//     values.push(apartment[key]);
+//   }
+    
+//   // Change code above this line
+// }
+// console.log(keys);
+// console.log(values);
+const products = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
+
+
+function getProductPrice(productName) {
+  // Change code below this line
+for (let product of products) {
+  if ("Scanner" === product.name) {
+     console.log(product.price);
+  }
+ 
+}
+
+  // Change code above this line
 }
